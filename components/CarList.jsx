6b9fa43d013aa6ff8d84ea0d30082f2097ed58e9
Adrian Quijivix <quijivix46@gmail.com>
2023-10-26 11:49:55 -0600

    Revert "carmodal"
    This reverts commit ec50652d490b9ee5af3b6739f9b9c546e97a8f67.

--- components/CarList.jsx
+++ components/CarList.jsx
@@ -1,24 +1,15 @@
-import CarCard from "../components/CarCard";
-import CarModal from '../components/CarModal'
-import {useState} from 'react'
+import CarCard from '../components/CarCard'
 
 const CarList = (props) => {
-
-  const [selectedCar, setSelectedCar] = useState()
-  
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {props.carsList.map((car, index) => (
-        <div key={index} onClick={()=>{document.getElementById("my_modal_4").showModal(); setSelectedCar(car)}}>
-          <CarCard car={car}/>
-        </div>
-      ))}
-     
-      <dialog id="my_modal_4" className="modal">
-        <CarModal car = {selectedCar}/>
-      </dialog>
+    <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        {props.carsList.map((car, index)=>(
+            <div key={index}>
+                <CarCard car = {car}/>
+            </div>
+        ))}
     </div>
-  );
-};
+  )
+}
 
-export default CarList;
+export default CarList
